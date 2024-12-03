50f8053fd06e31a10168cd0b41267550b3add0d9
rylCodes <andresdaryl.94@gmail.com>
2024-12-03 22:26:48 +0800

    chore: improve inventory report page

--- src/scenes/customer/daily.jsx
+++ src/scenes/customer/daily.jsx
@@ -1,23 +1,52 @@
-import { useContext, useEffect, useState } from "react";
-import { Button, Box, useTheme } from "@mui/material";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  Button,
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  useTheme,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import { tokens } from "../../theme";
-import { mockDataDaily } from "../../data/mockData";
+import { mockDataCustomer } from "../../data/mockData";
+import Header from "../../components/Header";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
-
-import Header from "../../components/Header";
+import AddCustomer from "./addCustomer";
+import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useAlert } from "../../context/AlertContext";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Daily = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
-  const [daily, setDaily] = useState(mockDataDaily);
-
   const { authToken } = useContext(AuthContext);
+  const showAlert = useAlert();
   const navigate = useNavigate();
+
+  const handleUpdateSuccess = () => {
+    showAlert(`Customer successfully updated.`, "success");
+  };
+
+  const handleArchiveSuccess = () => {
+    showAlert(`Customer successfully archived.`, "success");
+  };
+
+  const handleError = () => {
+    showAlert("An error occurred!", "error");
+  };
+
+  useEffect(() => {
+    fetchClients();
+  }, []);
 
   useEffect(() => {
     if (!authToken) {
@@ -25,15 +54,168 @@ const Daily = () => {
     }
   }, [authToken, navigate]);
 
-  const handleupdate = (id) => {
-    // Delete employee logic
-    setDaily((prevtDaily) => prevtDaily.filter((daily) => daily.id !== id));
+  const [AddCustomerOpen, setAddCustomerOpen] = useState(false);
+  const [updateCustomerOpen, setUpdateCustomerOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [customer, setCustomer] = useState([]);
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+
+  const handleOpen = () => setAddCustomerOpen(true);
+  const handleClose = () => setAddCustomerOpen(false);
+
+  const handleAddCustomer = (newcustomer) => {
+    fetchClients();
   };
 
-  const handleEdit = (id) => {
-    // Handle edit functionality
-    console.log("Edit employee with ID:", id);
-    // You can open a modal with pre-filled employee data for editing
+  const handleUpdateOpen = (customer) => {
+    const [firstname, ...lastname] = customer.name.split(" ");
+    setSelectedCustomer({
+      ...customer,
+      firstname,
+      lastname: lastname.join(" "),
+    });
+    setUpdateCustomerOpen(true);
+  };
+
+  const handleUpdateClose = () => {
+    setSelectedCustomer(null);
+    setUpdateCustomerOpen(false);
+  };
+
+  const handleArchive = async (id) => {
+    try {
+      const userConfirmed = confirm(
+        "Do you want to move this customer to archive?"
+      );
+      if (!userConfirmed) return;
+
+      const response = await axios.post(
+        `http://localhost:8000/api/admin/soft-delete-client/${id}`,
+        null, // No payload needed
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        handleArchiveSuccess();
+        fetchClients();
+      } else {
+        handleError();
+      }
+    } catch (error) {
+      console.error("Error deleting customer:", error);
+      handleError();
+    }
+  };
+
+  const fetchClients = async () => {
+    setIsFetching(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/api/staff/exercise-transaction/show",
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      console.log(response.data.data);
+      return;
+      if (response.status === 200) {
+        const clients = response.data.data.map((client) => ({
+          id: client.id,
+          name: client.fullname,
+          sex: client.gender,
+          email: client.email,
+          contact: client.contact_no,
+          address: client.address,
+          chosenservices: client.chosen_services,
+          instructor: client.instructor,
+          plan: client.plan,
+          amount: client.amount,
+          isActive: client.is_active,
+        }));
+        setCustomer(clients);
+        setIsFetching(false);
+      } else {
+        console.error("Failed to fetch clients");
+        setIsFetching(false);
+      }
+    } catch (error) {
+      console.error("Error fetching clients:", error);
+      setIsFetching(false);
+    }
+  };
+
+  // Update customer in the API
+  const handleUpdateCustomer = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(
+        `http://localhost:8000/api/admin/update-client/${selectedCustomer.id}`,
+        {
+          firstname: selectedCustomer.firstname,
+          lastname: selectedCustomer.lastname,
+          email: selectedCustomer.email,
+          password: selectedCustomer.password, // Optional: Only if changing password
+          address: selectedCustomer.address,
+          gender: selectedCustomer.sex,
+          contact_no: selectedCustomer.contact,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        // Update local state after successful API update
+        const updatedCustomer = {
+          ...selectedCustomer,
+          fullname:
+            selectedCustomer.firstname + " " + selectedCustomer.lastname,
+          password: "", // empty password
+        };
+
+        setCustomer((prevCustomer) =>
+          prevCustomer.map((c) =>
+            c.id === selectedCustomer.id ? updatedCustomer : c
+          )
+        );
+        handleUpdateClose();
+        setIsLoading(false);
+        handleUpdateSuccess();
+      } else {
+        setIsLoading(false);
+        handleError();
+      }
+    } catch (error) {
+      console.error("Error updating customer:", error);
+      setIsLoading(false);
+      handleError();
+    }
+  };
+
+  const serviceOptions = [
+    "Gym Per session",
+    "Gym Monthly",
+    "Monthly Treadmill",
+    "Gym + Treadmill",
+    "P.I Per Session",
+    "P.I Monthly",
+    "Zumba",
+    "Dance",
+    "Muay Thai",
+    "Taekwondo",
+    "Boxing",
+  ];
+
+  const handleSubmit = () => {
+    navigate("/payment-form", { state: { customer: selectedCustomer } });
   };
 
   const columns = [
@@ -47,28 +229,19 @@ const Daily = () => {
     { field: "instructor", headerName: "Instructor", flex: 1 },
     { field: "plan", headerName: "Plan", flex: 1 },
     { field: "amount", headerName: "Amount", flex: 1 },
-
     {
       field: "action",
       headerName: "Action",
       flex: 1,
       renderCell: (params) => (
-        <Box display="flex" gap="15px" justifyContent="center">
+        <Box display="flex" gap="9px" justifyContent="center">
           <Button
             variant="outlined"
-            color="inherit"
-            startIcon={<ArchiveOutlinedIcon />}
-            onClick={() => handleEdit(params.row.id)}
+            color="error"
+            startIcon={<DeleteIcon />}
+            onClick={() => handleArchive(params.row.id)}
           >
-            Archive
-          </Button>
-          <Button
-            variant="outlined"
-            color="success"
-            startIcon={<EditOutlinedIcon />}
-            onClick={() => handleupdate(params.row.id)}
-          >
-            Update
+            Remove
           </Button>
         </Box>
       ),
@@ -77,20 +250,14 @@ const Daily = () => {
 
   return (
     <Box m="20px">
-      <Header title="Daily" subtitle="Managing the daily Members" />
+      <Header title="Customer" subtitle="Managing the Customer Members" />
       <Box
         m="40px 0 0 0"
         height="75vh"
         sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
-          },
+          "& .MuiDataGrid-root": { border: "none" },
+          "& .MuiDataGrid-cell": { borderBottom: "none" },
+          "& .name-column--cell": { color: colors.greenAccent[300] },
           "& .MuiDataGrid-columnHeaders": {
             backgroundColor: colors.blueAccent[700],
             borderBottom: "none",
@@ -107,7 +274,128 @@ const Daily = () => {
           },
         }}
       >
-        <DataGrid checkboxSelection rows={daily} columns={columns} />
+        <Box display="flex" justifyContent="flex-end" mb="10px">
+          <Button variant="contained" color="primary" onClick={handleOpen}>
+            Add Customer
+          </Button>
+        </Box>
+
+        <DataGrid
+          loading={isFetching}
+          checkboxSelection
+          rows={customer}
+          columns={columns}
+        />
+
+        <Dialog
+          open={AddCustomerOpen}
+          onClose={handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <DialogContent>
+            <AddCustomer
+              closeModal={handleClose}
+              onAddCustomer={handleAddCustomer}
+            />
+          </DialogContent>
+        </Dialog>
+        <Dialog
+          open={updateCustomerOpen}
+          onClose={handleUpdateClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <DialogTitle>Update Customer</DialogTitle>
+          <DialogContent>
+            {selectedCustomer && (
+              <Box component="form" mt={2}>
+                <TextField
+                  fullWidth
+                  label="First Name"
+                  margin="normal"
+                  value={selectedCustomer.firstname}
+                  onChange={(e) =>
+                    setSelectedCustomer({
+                      ...selectedCustomer,
+                      firstname: e.target.value,
+                    })
+                  }
+                />
+                <TextField
+                  fullWidth
+                  label="Last Name"
+                  margin="normal"
+                  value={selectedCustomer.lastname}
+                  onChange={(e) =>
+                    setSelectedCustomer({
+                      ...selectedCustomer,
+                      lastname: e.target.value,
+                    })
+                  }
+                />
+                <TextField
+                  fullWidth
+                  label="Email"
+                  margin="normal"
+                  value={selectedCustomer.email}
+                  onChange={(e) =>
+                    setSelectedCustomer({
+                      ...selectedCustomer,
+                      email: e.target.value,
+                    })
+                  }
+                />
+                <TextField
+                  fullWidth
+                  label="Password"
+                  margin="normal"
+                  value={selectedCustomer.password}
+                  type="password"
+                  onChange={(e) =>
+                    setSelectedCustomer({
+                      ...selectedCustomer,
+                      password: e.target.value,
+                    })
+                  }
+                />
+                <TextField
+                  fullWidth
+                  label="Address"
+                  margin="normal"
+                  value={selectedCustomer.address}
+                  onChange={(e) =>
+                    setSelectedCustomer({
+                      ...selectedCustomer,
+                      address: e.target.value,
+                    })
+                  }
+                />
+                <TextField
+                  fullWidth
+                  label="Contact No"
+                  margin="normal"
+                  value={selectedCustomer.contact}
+                  onChange={(e) =>
+                    setSelectedCustomer({
+                      ...selectedCustomer,
+                      contact: e.target.value,
+                    })
+                  }
+                />
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleUpdateCustomer}
+                  disabled={isLoading}
+                >
+                  Save
+                </Button>
+              </Box>
+            )}
+          </DialogContent>
+        </Dialog>
       </Box>
     </Box>
   );
